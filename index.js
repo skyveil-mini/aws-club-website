8b84f6b9f36101ff3e9ea86c5e4a1f8f064e4d45
skyveil-mini <adwid1234@gmail.com>
2024-10-19 17:55:00 +0530

    Added scrolling animations and auto scroller

--- index.js
+++ index.js
@@ -4,7 +4,40 @@
 console.log("Welcome to a happy place :)");
 
 // Auto scroller
+const perks_list = document.querySelector('.perk-list');
+const translate_speed = 50;
+const first_child = perks_list.children.item(0);
+const perks = document.querySelector('.perks');
 
+let position = 0;
+
+setInterval(() => {
+    position += translate_speed;
+    if (first_child.getBoundingClientRect().x > perks.getBoundingClientRect().x)
+        position = 0;
+
+    perks_list.animate(
+        [
+            { transform: `translateX(${position}px)`}
+        ],
+        {
+            duration: 1000,
+            fill: "forwards",
+            easing: 'ease-in-out'
+        }
+    )
+}, 2000);
+
+// Scroll in animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) entry.target.classList.add('show');
+        else entry.target.classList.remove('show');
+    })
+})
+
+const hidden_elements = document.querySelectorAll('.hidden');
+hidden_elements.forEach((element) => observer.observe(element));
 
 // Parllax effect
 //  TODO:
