08afe51c6729f303a6803e7c579c678ab5449f50
skyveil-mini <adwid1234@gmail.com>
2024-10-19 16:33:21 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Haven't cluttered the css or html with too many tags and styles so as to keep things readable
+// Most of the "Dynamic" stuff on the website is done with javascript
+
+console.log("Welcome to a happy place :)");
+
+// Auto scroller
+
+
+// Parllax effect
+//  TODO:
+const handle = (tag, speed) => {
+    let element = document.querySelector(tag);
+
+    window.addEventListener('scroll', () => {
+        let distance = window.scrollY;
+    })
+}
